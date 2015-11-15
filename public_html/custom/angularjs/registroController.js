da037486f6483837e95daadbe759932fd82c02f9
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-11-15 16:18:09 +0100

    Creada funcion de registro + realimentacion

--- public_html/custom/angularjs/registroController.js
+++ public_html/custom/angularjs/registroController.js
@@ -47,10 +47,10 @@ ClubPadelApp.controller("registroController", function ($scope, $http) {
                 e.closest(".form-group").removeClass("has-error");
             }, submitHandler: function (e) {
                 //Validamos el drag and drop
-                if (validationDragDrop()) {
-                    //Hacemos el submit del formulario con la llamada a la funcion
-                    $scope.registro();
-                }
+//                if (validationDragDrop()) {
+                //Hacemos el submit del formulario con la llamada a la funcion
+                $scope.registro();
+//                }
             }});
     }
     $scope.existeUsuario = function () {
@@ -106,6 +106,86 @@ ClubPadelApp.controller("registroController", function ($scope, $http) {
             return true;
         });
     }
+
+
+    $scope.registro = function () {
+        var idUsuario = $("#idUsuario").val();
+        var correo = $("#correo").val();
+        var clave = $("#clave").val();
+        var fechaNacimiento = $("#fechaNacimiento").val();
+        $.ajax({
+            method: "POST",
+            dataType: "json",
+            url: "http://salonso.etsisi.upm.es/miw_serv/padel/usuario.php",
+            data: {
+                "username": idUsuario,
+                "email": correo,
+                "password": clave,
+                "birthDate": fechaNacimiento,
+            },
+            success: function (datos) {
+                console.log(datos)
+                var error = datos.errorMessage;
+                console.log(error)
+                if (error.indexOf("no error") > -1) { //ok
+                    //Mostramos la realimentacion
+                    $(".alert-danger").addClass("hidden");
+                    $(".alert-success").removeClass("hidden");
+                    return false;
+                } else {//error
+                    //Mostramos la realimentacion
+                    $(".alert-success").addClass("hidden");
+                    $(".alert-danger").removeClass("hidden");
+                    $(".alert-danger").html("<strong>¡Error!</strong> " + error);
+                    return true;
+                }
+                //Mostramos el error por consola
+                console.log("REGISTRO = " + error);
+            },
+            error: function (datos) {
+                console.log("error peticion");
+                return true;
+            }
+        });
+    }
+//    $scope.registro = function () {
+//        var idUsuario = $("#idUsuario").val();
+//        var correo = $("#correo").val();
+//        var clave = $("#clave").val();
+//        var fechaNacimiento = $("#fechaNacimiento").val();
+//        $http({
+//            method: "POST",
+//            url: "http://salonso.etsisi.upm.es/miw_serv/padel/usuario.php",
+//            data: {
+//                "username": idUsuario,
+//                "email": correo,
+//                "password": clave,
+//                "birthDate": fechaNacimiento,
+//            },
+//            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+//        }).success(function (datos, status, headers, config) {
+//            console.log(datos);
+//            console.log(status);
+//            console.log(headers);
+//            console.log(config);
+//            var respuesta = angular.fromJson(datos);
+//            console.log(respuesta);
+//            var error = respuesta.errorMessage;
+//            console.log(error);
+//            if (error === "no error") { //no existe
+//                $("#idUsuario").closest(".form-group").removeClass("has-error");
+//                return false;
+//            } else {//si existe
+//                $("#idUsuario").closest(".form-group").addClass("has-error");
+//                return true;
+//            }
+//            //Mostramos el error por consola
+//            console.log("ERROR = " + error);
+//        }).error(function (datos, status, headers, config) {
+//            console.log("error peticion");
+//            return true;
+//        });
+//    }
 });
 
 
